bc28d2cec8fc36a0f8e66bd936436d0d268091c0
sheum <nazmulhasansheum@gmail.com>
2026-03-14 11:58:14 +0600

    Feat: Add activity logger for audit purpose and central service to track log

---
+++ src/app/Services/ActivityLogger.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class ActivityLogger
+{
+    public static function log($description, $user = null, $properties = [])
+    {
+        activity()
+            ->causedBy($user)
+            ->withProperties(array_merge([
+                'ip' => request()->ip(),
+                'url' => request()->fullUrl(),
+                'user_agent' => request()->userAgent()
+            ], $properties))
+            ->log($description);
+    }
+}
